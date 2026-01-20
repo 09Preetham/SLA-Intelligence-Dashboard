3a955d71a92a82e7a95e4a041b181a3ddae6e17f
09Preetham <105037258+09Preetham@users.noreply.github.com>
2026-01-20 20:24:57 +0530

    Update 01_data_validation.py

--- 01_data_validation.py
+++ 01_data_validation.py
@@ -1,3 +1,10 @@
+"""
+Project: SLA Intelligence Dashboard
+Description: Analyzes impact of error frequency on SLA breaches
+Author: V S M Preetham NAIDU
+"""
+
+
 import pandas as pd
 
 df = pd.read_csv("customer_support_data.csv")
@@ -12,3 +19,4 @@ df["Calculated_SLA"] = df["TAT_Hours"] <= df["SLA_Hours"]
 df["Calculated_SLA"] = df["Calculated_SLA"].map({True:"Yes", False:"No"})
 
 print((df["Calculated_SLA"] == df["SLA_Met"]).value_counts())
+
