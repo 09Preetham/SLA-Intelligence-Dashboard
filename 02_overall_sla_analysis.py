8ea3352287c47500ee93331b57bdb19b4e8e43fa
09Preetham <105037258+09Preetham@users.noreply.github.com>
2026-01-20 20:25:23 +0530

    Update 02_overall_sla_analysis.py

--- 02_overall_sla_analysis.py
+++ 02_overall_sla_analysis.py
@@ -1,6 +1,13 @@
+"""
+Project: SLA Intelligence Dashboard
+Description: Analyzes impact of error frequency on SLA breaches
+Author: V S M Preetham NAIDU
+"""
+
 import pandas as pd
 
 df = pd.read_csv("customer_support_data.csv")
 
 sla_dist = df["SLA_Met"].value_counts(normalize=True) * 100
 print(sla_dist)
+
